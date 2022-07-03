890040d7b20b0ea87cc3073bc48e24930e4f278b
137ryo <ryo167prog@gmail.com>
2022-07-03 22:37:31 +0900

    Initial commit - mybatis generator setup

---
+++ src/main/kotlin/com/example/subscriptionmanage/SubscriptionManageApplication.kt
@@ -0,0 +1,11 @@
+package com.example.subscriptionmanage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionManageApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubscriptionManageApplication>(*args)
+}
